119896abedb7ec4aa3ee53a064d0f07957b9b2d8
Tilen Majerle <tilen.majerle@gmail.com>
2016-10-23 21:06:36 +0200

    GPS first commit

---
+++ 00-GPS_LIBRARY/gps.h
@@ -0,0 +1,307 @@
+/**
+ * \author  Tilen Majerle
+ * \email   tilen@majerle.eu
+ * \website http://majerle.eu/projects/
+ * \license MIT
+ * \brief   GPS Library
+ *	
+\verbatim
+   ----------------------------------------------------------------------
+    Copyright (c) 2016 Tilen Majerle
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software, 
+    and to permit persons to whom the Software is furnished to do so, 
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+    AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+   ----------------------------------------------------------------------
+\endverbatim
+ */
+#ifndef GPS_H
+#define GPS_H 010
+
+/* C++ detection */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \defgroup GPS
+ * \brief  Platform independent, written in ANSII C, GPS AT parser library for SIMcom modules   
+ * \{
+ *
+ * \par Features
+ *
+\verbatim
+- Platform independant GPS NMEA Parser library
+- Written in ANSII C89
+- Low-level layer separated from application layer, thus allows you to write custom communication for GPS device
+- Allows you to add custom GPS statement which are not originally added to library
+- By default parses GPGGA, GPRMC, GPGSA and GPGSV statements which must exist in GPS output
+\endverbatim
+ *
+ * \par Download
+ * 
+ * Download and full source code with examples can be found on official <a href="https://github.com/MaJerle/GPS_NMEA_parser">Github repository</a>.
+ */
+#include "stdlib.h"
+#include "string.h"
+#include "stdint.h"
+#include "stdio.h"
+#include "math.h"
+
+/* Buffer implementation */
+#include "buffer.h"
+
+/* GPS configuration */
+#include "gps_config.h"
+
+/**
+ * @defgroup GPS_Macros
+ * @brief    Library defines
+ * @{
+ */
+
+/**
+ * \}
+ */
+ 
+/**
+ * \defgroup GPS_Typedefs
+ * \brief    Library Typedefs
+ * \{
+ */
+
+/**
+ * \brief  GPS library result enumeration
+ */
+typedef enum _GPS_Result_t {
+	gpsOK,                              /*!< OK */
+	gpsERROR,                           /*!< An error occurred */
+    gpsNODATA,                          /*!< No data from GPS */
+	gpsOLDDATA,                         /*!< An old data still remaining */
+	gpsNEWDATA                          /*!< New pack of fresh data just received */
+} GPS_Result_t;
+
+/**
+ * \brief  GPS Fix type
+ */
+typedef enum _GPS_Fix_t {
+	GPS_Fix_Invalid = 0x00,             /*!< No fix */
+	GPS_Fix_GPS = 0x01,                 /*!< Fix is GPS */
+	GPS_Fix_DGPS = 0x02                 /*!< Fix is Differential GPS */
+} GPS_Fix_t;
+
+/**
+ * \brief  GPS Fix mode
+ */
+typedef enum GPS_FixMode_t {
+	GPS_FixMode_Invalid = 0x01,         /*!< No fix */
+	GPS_FixMode_2D = 0x02,              /*!< 2D fix */
+	GPS_FixMode_3D = 0x03               /*!< 3D fix */
+} GPS_FixMode_t;
+
+/**
+ * \brief  GPS satellite description
+ */
+typedef struct _GPS_Sat_t {
+	uint8_t ID;                         /*!< Satellite ID */
+	uint8_t Elevation;                  /*!< Elevation */
+	uint16_t Azimuth;                   /*!< Azimuth according to north */
+	uint8_t SNR;                        /*!< Signal to noise ration in units of dB */
+} GPS_Sat_t;
+
+/**
+ * \brief  GPS time
+ */
+typedef struct _GPS_Time_t {
+	uint8_t Hours;
+	uint8_t Minutes;
+	uint8_t Seconds;
+	uint8_t Hundreds;
+	uint16_t Thousands;
+} GPS_Time_t;
+
+/**
+ * \brief  GPS date
+ */
+typedef struct _GPS_Date_t {
+	uint8_t Day;
+	uint8_t Month;
+	uint8_t Year;
+} GPS_Date_t;
+
+/**
+ * \brief  GPS speed enumeration
+ */
+typedef enum _GPS_Speed_t {
+	/* Metric */
+	GPS_Speed_KilometerPerSecond,		/*!< Convert speed to kilo meters per second */
+	GPS_Speed_MeterPerSecond,			/*!< Convert speed to meters per second */
+	GPS_Speed_KilometerPerHour,			/*!< Convert speed to kilo meters per hour */
+	GPS_Speed_MeterPerMinute,			/*!< Convert speed to meter per minute */
+	/* Imperial */
+	GPS_Speed_MilePerSecond,			/*!< Convert speed to miles per second */
+	GPS_Speed_MilePerHour,				/*!< Convert speed to miles per hour */
+	GPS_Speed_FootPerSecond,			/*!< Convert speed to foots per second */
+	GPS_Speed_FootPerMinute,			/*!< Convert speed to foots per minute */
+	/* For Runners and Joggers */
+	GPS_Speed_MinutePerKilometer,		/*!< Convert speed to minutes per kilo meter */
+	GPS_Speed_SecondPerKilometer,		/*!< Convert speed to seconds per kilo meter */
+	GPS_Speed_SecondPer100Meters,		/*!< Convert speed to seconds per 100 meters */
+	GPS_Speed_MinutePerMile,			/*!< Convert speed to minutes per mile */
+	GPS_Speed_SecondPerMile,			/*!< Convert speed to seconds per mile */
+	GPS_Speed_SecondPer100Yards,		/*!< Convert speed to seconds per 100 yards */
+	/* Nautical */
+	GPS_Speed_SeaMilePerHour,			/*!< Convert speed to sea miles per hour */
+} GPS_Speed_t;
+
+/**
+ * \brief  GPS Distance and bearing
+ */
+typedef struct _GPS_Distance_t  {
+	float Latitude1;					/*!< Latitude of starting point. */
+	float Longitude1; 					/*!< Longitude of starting point. */
+	float Latitude2;  					/*!< Latitude of ending point. */
+	float Longitude2; 					/*!< Longitude of ending point. */
+	float Distance;   					/*!< Distance between 2 points which will be calculated. */
+	float Bearing;    					/*!< Bearing from start to stop point according to North. */
+} GPS_Distance_t;
+
+typedef struct _GPS_Custom_t {
+	const char* Statement;				/*!< Statement value, including "$" at beginning. For example, "$GPRMC" */
+	uint8_t TermNumber;					/*!< Term number position inside statement */
+	char Value[13];						/*!< Value from GPS receiver at given statement and term number will be stored here.
+												\note Value will not be converted to number if needed, but will stay as a character */
+	uint8_t Updated;					/*!< Updated flag. If this parameter is set to 1, then new update has been made. Meant for private use */
+} GPS_Custom_t;
+
+/**
+ * \brief  GPS working structure
+ */
+typedef struct _GPS_t {
+	/* GPGGA statement */
+	float Latitude;                     /*!< GPS Latitude value */
+	float Longitude;                    /*!< GPS Longitude value */
+	float Altitude;                     /*!< GPS Altitude value */
+	GPS_Fix_t Fix;                      /*!< GPS Fix type */
+	uint8_t SatsInUse;                  /*!< Number of satellites in use for fix */
+	GPS_Time_t Time;                    /*!< UTC Time */
+
+	/* GPGSA statement */
+	GPS_FixMode_t FixMode;              /*!< GPS Fix mode */
+	uint8_t SatelliteIDs[12];           /*!< ID numbers of satellites in use. Valid range is from 0 to SatsInUse - 1*/
+	float HDOP;                         /*!< Horizontal dilution of precision */
+	float PDOP;                         /*!< Position dilution of precision */
+	float VDOP;                         /*!< Vertical dilution of precision */
+
+	/* GPGSV statement */
+	uint8_t SatsInView;                 /*!< Satellites in view */
+	GPS_Sat_t SatsDesc[24];             /*!< Array of descriptions for satellites in view */
+
+	/* GPRMC statement */
+	GPS_Date_t Date;                    /*!< UTC date */
+	uint8_t Valid;                      /*!< GPS valid status */
+	float Speed;                        /*!< Current speed in knots */
+	float Coarse;                       /*!< Current coarse on ground */
+	float Variation;                    /*!< Variation value */
+
+	/* Private data */
+	GPS_Custom_t* CustomStatements[GPS_CUSTOM_COUNT];   /*!< Array of pointers to custom GPS statements */
+	uint8_t CustomStatementsCount;      /*!< Number of custom GPS statements */
+} GPS_t;
+
+/**
+* \}
+*/
+
+/**
+* \defgroup GPS_Functions
+* \brief    Library functions
+* \{
+*/
+
+/**
+ * \brief  Initializes GPS stack
+ * \param  *GPS: Pointer to working \ref GPS_t structure
+ * \retval Member of \ref GPS_Result_t enumeration
+ */
+GPS_Result_t GPS_Init(GPS_t* GPS);
+
+/**
+ * \brief  Adds new data to GPS stack
+ * \note   This function must be called when new data are received from GPS. Use it as input method for GPS stack
+ * \param  *ch: Pointer to characters to write to stack
+ * \param  count: Number of characters to write at a time
+ * \retval Number of characters written to buffer
+ */
+uint32_t GPS_DataReceived(uint8_t* ch, size_t count);
+
+/**
+ * \brief  Process update GPS stack
+ * \param  *GPS: Pointer to working \ref GPS_t structure
+ * \retval Member of \ref GPS_Result_t enumeration
+ */
+GPS_Result_t GPS_Update(GPS_t* GPS);
+
+/**
+ * \brief  Converts speed in knots (from GPS) to user selectable speed
+ * \param  speedInKnots: float value from GPS module
+ * \param  toSpeed: Select to which speed you want conversion from knot. This parameter ca be a value of TM_GPS_Speed_t enumeration.
+ * \retval Calculated speed from knots to user selectable format
+ */
+float GPS_ConvertSpeed(float SpeedInKnots, GPS_Speed_t toSpeed);
+
+/**
+ * \brief  Calculates distance between 2 coordinates on earth and bearing from start to end point in relation to the north
+ * \note   Calculation results will be saved in *Distance_Data @ref TM_GPS_Distance_t structure
+ * \param  *Distance: Pointer to @ref TM_GPS_Distance_t structure with latitude and longitude set values
+ * \retval Member of \ref GPS_Result_t enumeration
+ */
+GPS_Result_t GPS_DistanceBetween(GPS_Distance_t* Distance);
+
+/**
+ * \brief  Adds custom GPGxx statement to array of user selectable statements.
+ *            Array is available to user using \ref TM_GPS_t working structure
+ * \note   Also note, that your GPS receiver HAVE TO send statement type you use in this function, or
+ *            \ref GPS_Update function will always return that there is no new data available to read
+ * \param  *GPS: Pointer to working \ref GPS_t structure
+ * \param  *Custom: Pointer to empty \ref GPS_Custom_t structure
+ * \param  *GPG_Statement: String of NMEA starting line address, including "$" at beginning
+ * \param  TermNumber: Position in NMEA statement
+ * \retval Member of \ref GPS_Result_t enumeration
+ */
+GPS_Result_t GPS_Custom_Add(GPS_t* GPS, GPS_Custom_t* Custom, const char* GPG_Statement, uint8_t TermNumber);
+
+/**
+ * \}
+ */
+
+/**
+ * \}
+ */
+ 
+/**
+ * \}
+ */
+
+/* C++ detection */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ 00-STM32_LIBRARIES/tm_stm32_buffer.c
@@ -0,0 +1,417 @@
+/**	
+ * |----------------------------------------------------------------------
+ * | Copyright (C) Tilen Majerle, 2015
+ * | 
+ * | This program is free software: you can redistribute it and/or modify
+ * | it under the terms of the GNU General Public License as published by
+ * | the Free Software Foundation, either version 3 of the License, or
+ * | any later version.
+ * |  
+ * | This program is distributed in the hope that it will be useful,
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * | GNU General Public License for more details.
+ * | 
+ * | You should have received a copy of the GNU General Public License
+ * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * |----------------------------------------------------------------------
+ */
+#include "tm_stm32_buffer.h"
+
+uint8_t TM_BUFFER_Init(TM_BUFFER_t* Buffer, uint16_t Size, uint8_t* BufferPtr) {
+	/* Set buffer values to all zeros */
+	memset(Buffer, 0, sizeof(TM_BUFFER_t));
+	
+	/* Set default values */
+	Buffer->Size = Size;
+	Buffer->Buffer = BufferPtr;
+	Buffer->StringDelimiter = '\n';
+	
+	/* Check if malloc should be used */
+	if (!Buffer->Buffer) {
+		/* Try to allocate */
+		Buffer->Buffer = (uint8_t *) LIB_ALLOC_FUNC(Size * sizeof(uint8_t));
+		
+		/* Check if allocated */
+		if (!Buffer->Buffer) {
+			/* Reset size */
+			Buffer->Size = 0;
+			
+			/* Return error */
+			return 1;
+		} else {
+			/* Set flag for malloc */
+			Buffer->Flags |= BUFFER_MALLOC;
+		}
+	}
+	
+	/* We are initialized */
+	Buffer->Flags |= BUFFER_INITIALIZED;
+	
+	/* Initialized OK */
+	return 0;
+}
+
+void TM_BUFFER_Free(TM_BUFFER_t* Buffer) {
+	/* Check buffer structure */
+	if (Buffer == NULL) {
+		return;
+	}
+	
+	/* If malloc was used for allocation */
+	if (Buffer->Flags & BUFFER_MALLOC) {
+		/* Free memory */
+		LIB_FREE_FUNC(Buffer->Buffer);
+	}
+	
+	/* Clear flags */
+	Buffer->Flags = 0;
+	Buffer->Size = 0;
+}
+
+uint16_t TM_BUFFER_Write(TM_BUFFER_t* Buffer, const uint8_t* Data, uint16_t count) {
+	uint8_t i = 0;
+	
+	/* Check buffer structure */
+	if (Buffer == NULL) {
+		return 0;
+	}
+
+	/* Check input pointer */
+	if (Buffer->In >= Buffer->Size) {
+		Buffer->In = 0;
+	}
+	
+	/* Go through all elements */
+	while (count--) {
+		/* Check if buffer full */
+		if (
+			(Buffer->In == (Buffer->Out - 1)) ||
+			(Buffer->Out == 0 && Buffer->In == (Buffer->Size - 1))
+		) {
+			break;
+		}
+		
+		/* Add to buffer */
+		Buffer->Buffer[Buffer->In++] = *Data++;
+		
+		/* Increase pointers */
+		i++;
+		
+		/* Check input overflow */
+		if (Buffer->In >= Buffer->Size) {
+			Buffer->In = 0;
+		}
+	}
+	
+	/* Return number of elements stored in memory */
+	return i;
+}
+
+uint16_t TM_BUFFER_Read(TM_BUFFER_t* Buffer, uint8_t* Data, uint16_t count) {
+	uint16_t i = 0;
+	
+	/* Check buffer structure */
+	if (Buffer == NULL) {
+		return 0;
+	}
+
+	/* Check output pointer */
+	if (Buffer->Out >= Buffer->Size) {
+		Buffer->Out = 0;
+	}
+	
+	/* Go through all elements */
+	while (count--) {
+		/* Check if pointers are same = buffer is empty */
+		if (Buffer->Out == Buffer->In) {
+			break;
+		}
+		
+		/* Save to user buffer */
+		*Data++ = Buffer->Buffer[Buffer->Out++];
+		
+		/* Increase pointers */
+		i++;
+
+		/* Check output overflow */
+		if (Buffer->Out >= Buffer->Size) {
+			Buffer->Out = 0;
+		}
+	}
+	
+	/* Return number of elements read from buffer */
+	return i;
+}
+
+uint16_t TM_BUFFER_GetFree(TM_BUFFER_t* Buffer) {
+	uint32_t size, in, out;
+	
+	/* Check buffer structure */
+	if (Buffer == NULL) {
+		return 0;
+	}
+	
+	/* Save values */
+	in = Buffer->In;
+	out = Buffer->Out;
+	
+	/* Check if the same */
+	if (in == out) {
+		size = Buffer->Size;
+	}
+
+	/* Check normal mode */
+	if (out > in) {
+		size = out - in;
+	}
+	
+	/* Check if overflow mode */
+	if (in > out) {
+		size = Buffer->Size - (in - out);
+	}
+	
+	/* Return free memory */
+	return size - 1;
+}
+
+uint16_t TM_BUFFER_GetFull(TM_BUFFER_t* Buffer) {
+	uint32_t in, out, size;
+	
+	/* Check buffer structure */
+	if (Buffer == NULL) {
+		return 0;
+	}
+	
+	/* Save values */
+	in = Buffer->In;
+	out = Buffer->Out;
+	
+	/* Pointer are same? */
+	if (in == out) {
+		size = 0;
+	}
+	
+	/* Check pointers and return values */
+	/* Buffer is not in overflow mode */
+	if (in > out) {
+		size = in - out;
+	}
+	
+	/* Buffer is in overflow mode */
+	if (out > in) {
+		size = Buffer->Size - (out - in);
+	}
+	
+	/* Return number of elements in buffer */
+	return size;
+}
+
+void TM_BUFFER_Reset(TM_BUFFER_t* Buffer) {
+	/* Check buffer structure */
+	if (Buffer == NULL) {
+		return;
+	}
+	
+	/* Reset values */
+	Buffer->In = 0;
+	Buffer->Out = 0;
+}
+
+int16_t TM_BUFFER_FindElement(TM_BUFFER_t* Buffer, uint8_t Element) {
+	uint16_t Num, Out, retval = 0;
+	
+	/* Check buffer structure */
+	if (Buffer == NULL) {
+		return -1;
+	}
+	
+	/* Create temporary variables */
+	Num = TM_BUFFER_GetFull(Buffer);
+	Out = Buffer->Out;
+	
+	/* Go through input elements */
+	while (Num > 0) {
+		/* Check output overflow */
+		if (Out >= Buffer->Size) {
+			Out = 0;
+		}
+		
+		/* Check for element */
+		if ((uint8_t)Buffer->Buffer[Out] == (uint8_t)Element) {
+			/* Element found, return position in buffer */
+			return retval;
+		}
+		
+		/* Set new variables */
+		Out++;
+		Num--;
+		retval++;
+	}
+	
+	/* Element is not in buffer */
+	return -1;
+}
+
+int16_t TM_BUFFER_Find(TM_BUFFER_t* Buffer, const uint8_t* Data, uint16_t Size) {
+	uint16_t Num, Out, i, retval = 0;
+	uint8_t found = 0;
+
+	/* Check buffer structure and number of elements in buffer */
+	if (Buffer == NULL || (Num = TM_BUFFER_GetFull(Buffer)) < Size) {
+		return -1;
+	}
+
+	/* Create temporary variables */
+	Out = Buffer->Out;
+
+	/* Go through input elements in buffer */
+	while (Num > 0) {
+		/* Check output overflow */
+		if (Out >= Buffer->Size) {
+			Out = 0;
+		}
+
+		/* Check if current element in buffer matches first element in data array */
+		if ((uint8_t)Buffer->Buffer[Out] == (uint8_t)Data[0]) {
+			found = 1;
+		}
+
+		/* Set new variables */
+		Out++;
+		Num--;
+		retval++;
+
+		/* We have found first element */
+		if (found) {
+			/* First character found */
+			/* Check others */
+			i = 1;
+			while (i < Size) {
+				/* Check output overflow */
+				if (Out >= Buffer->Size) {
+					Out = 0;
+				}
+
+				/* Check if current character in buffer matches character in string */
+				if ((uint8_t)Buffer->Buffer[Out] != (uint8_t)Data[i]) {
+					retval += i - 1;
+					break;
+				}
+
+				/* Set new variables */
+				Out++;
+				Num--;
+				i++;
+			}
+
+			/* We have found data sequence in buffer */
+			if (i == Size) {
+				return retval;
+			}
+		}
+	}
+
+	/* Data sequence is not in buffer */
+	return -1;
+}
+
+uint16_t TM_BUFFER_WriteString(TM_BUFFER_t* Buffer, const char* buff) {
+	/* Write string to buffer */
+	return TM_BUFFER_Write(Buffer, (uint8_t *)buff, strlen(buff));
+}
+
+uint16_t TM_BUFFER_ReadString(TM_BUFFER_t* Buffer, char* buff, uint16_t buffsize) {
+	uint16_t i = 0;
+	uint8_t ch;
+	uint16_t memFree, memFull;
+	
+	/* Check value buffer */
+	if (Buffer == NULL) {
+		return 0;
+	}
+	
+	/* Get free */
+	memFree = TM_BUFFER_GetFree(Buffer);
+	memFull = TM_BUFFER_GetFull(Buffer);
+	
+	/* Check for any data on USART */
+	if (
+		memFull == 0 ||                                                   /*!< Buffer empty */
+		(
+			TM_BUFFER_FindElement(Buffer, Buffer->StringDelimiter) < 0 && /*!< String delimiter is not in buffer */
+			memFree != 0 &&                                               /*!< Buffer is not full */
+			memFull < buffsize                                            /*!< User buffer size is larger than number of elements in buffer */
+		)
+	) {
+		/* Return 0 */
+		return 0;
+	}
+	
+	/* If available buffer size is more than 0 characters */
+	while (i < (buffsize - 1)) {
+		/* We have available data */
+		if (TM_BUFFER_Read(Buffer, &ch, 1) == 0) {
+			break;
+		}
+		
+		/* Save character */
+		buff[i] = (char)ch;
+		
+		/* Check for end of string */
+		if ((char)buff[i] == (char)Buffer->StringDelimiter) {
+			/* Done */
+			break;
+		}
+		
+		/* Increase */
+		i++;
+	}
+	
+	/* Add zero to the end of string */
+	if (i == (buffsize - 1)) {
+		buff[i] = 0;
+	} else {
+		buff[++i] = 0;
+	}
+
+	/* Return number of characters in buffer */
+	return i;
+}
+
+int8_t TM_BUFFER_CheckElement(TM_BUFFER_t* Buffer, uint16_t pos, uint8_t* element) {
+	uint16_t In, Out, i = 0;
+	
+	/* Check value buffer */
+	if (Buffer == NULL) {
+		return 0;
+	}
+	
+	/* Read current values */
+	In = Buffer->In;
+	Out = Buffer->Out;
+	
+	/* Set pointers to right location */
+	while (i < pos && (In != Out)) {
+		/* Increase output pointer */
+		Out++;
+		i++;
+		
+		/* Check overflow */
+		if (Out >= Buffer->Size) {
+			Out = 0;
+		}
+	}
+	
+	/* If positions match */
+	if (i == pos) {
+		/* Save element */
+		*element = Buffer->Buffer[Out];
+		
+		/* Return OK */
+		return 1;
+	}
+	
+	/* Return zero */
+	return 0;
+}
