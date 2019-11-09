bcc34a4f00ebf3b24ab43480ffa77dee9e301f11
Tilen Majerle <tilen.majerle@gmail.com>
2019-11-09 12:22:23 +0100

    Add v1.1.0 version

--- gps_nmea_parser/src/include/gps/gps.h
+++ gps_nmea_parser/src/include/gps/gps.h
@@ -29,7 +29,7 @@
  * This file is part of GPS NMEA parser library.
  *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
- * Version:         $_version_$
+ * Version:         v1.1.0
  */
 #ifndef GPS_HDR_H
 #define GPS_HDR_H
