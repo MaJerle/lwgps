e606de28c2499192b544d70026834e826ed59a7e
Tilen Majerle <tilen.majerle@gmail.com>
2023-12-09 16:49:22 +0100

    Improve the cmake documentation

--- lwgps/library.cmake
+++ lwgps/library.cmake
@@ -1,3 +1,14 @@
+# 
+# This file provides set of variables for end user
+# and also generates one (or more) libraries, that can be added to the project using target_link_libraries(...)
+#
+# Before this file is included to the root CMakeLists file (using include() function), user can set some variables:
+#
+# LWGPS_OPTS_DIR: If defined, it should set the folder path where options file shall be generated.
+# LWGPS_COMPILE_OPTIONS: If defined, it provide compiler options for generated library.
+# LWGPS_COMPILE_DEFINITIONS: If defined, it provides "-D" definitions to the library build
+#
+
 # Library core sources
 set(lwgps_core_SRCS 
     ${CMAKE_CURRENT_LIST_DIR}/src/lwgps/lwgps.c
