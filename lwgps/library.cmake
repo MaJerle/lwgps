6d286839771c3675aadbfd52779a9abbcd0ec9e3
Tilen Majerle <tilen@majerle.eu>
2024-03-19 21:34:24 +0100

    Update library CMake system, to support automatic creation of options file, if user doesn't provide one

--- lwgps/library.cmake
+++ lwgps/library.cmake
@@ -1,13 +1,18 @@
 # 
+# LIB_PREFIX: LWGPS
+#
 # This file provides set of variables for end user
 # and also generates one (or more) libraries, that can be added to the project using target_link_libraries(...)
 #
 # Before this file is included to the root CMakeLists file (using include() function), user can set some variables:
 #
-# LWGPS_OPTS_DIR: If defined, it should set the folder path where options file shall be generated.
+# LWGPS_OPTS_FILE: If defined, it is the path to the user options file. If not defined, one will be generated for you automatically
 # LWGPS_COMPILE_OPTIONS: If defined, it provide compiler options for generated library.
 # LWGPS_COMPILE_DEFINITIONS: If defined, it provides "-D" definitions to the library build
 #
+
+# Custom include directory
+set(LWGPS_CUSTOM_INC_DIR ${CMAKE_CURRENT_BINARY_DIR}/lib_inc)
 
 # Library core sources
 set(lwgps_core_SRCS 
@@ -17,6 +22,7 @@ set(lwgps_core_SRCS
 # Setup include directories
 set(lwgps_include_DIRS
     ${CMAKE_CURRENT_LIST_DIR}/src/include
+    ${LWGPS_CUSTOM_INC_DIR}
 )
 
 # Register library to the system
@@ -26,7 +32,12 @@ target_include_directories(lwgps INTERFACE ${lwgps_include_DIRS})
 target_compile_options(lwgps PRIVATE ${LWGPS_COMPILE_OPTIONS})
 target_compile_definitions(lwgps PRIVATE ${LWGPS_COMPILE_DEFINITIONS})
 
-# Create config file
-if(DEFINED LWGPS_OPTS_DIR AND NOT EXISTS ${LWGPS_OPTS_DIR}/lwgps_opts.h)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/src/include/lwgps/lwgps_opts_template.h ${LWGPS_OPTS_DIR}/lwgps_opts.h COPYONLY)
+# Create config file if user didn't provide one info himself
+if(NOT LWGPS_OPTS_FILE)
+    message(STATUS "Using default lwgps_opts.h file")
+    set(LWGPS_OPTS_FILE ${CMAKE_CURRENT_LIST_DIR}/src/include/lwgps/lwgps_opts_template.h)
+else()
+    message(STATUS "Using custom lwgps_opts.h file from ${LWGPS_OPTS_FILE}")
 endif()
+configure_file(${LWGPS_OPTS_FILE} ${LWGPS_CUSTOM_INC_DIR}/lwgps_opts.h COPYONLY)
+
